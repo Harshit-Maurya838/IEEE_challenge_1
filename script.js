4c56dc6ffc44ba8a759b1fce2ba3d1081a8df76d
Harshit-Maurya838 <44harshitmaurya@gmail.com>
2025-01-24 00:18:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Optional: Add interactivity if needed, such as animations or additional hover effects
+document.querySelector('.card').addEventListener('click', function() {
+    alert('Card clicked!');
+});
